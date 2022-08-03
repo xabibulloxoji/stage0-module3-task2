f0131a9ee36fb97af69d4b4f084d1325869b3d21
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-03 20:38:18 +0500

    Revert "done"
    This reverts commit 206945f09ccd3282934e6eba590ed7c4c964ab28.

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -8,40 +8,9 @@ public class advancedNamingConvention {
 
     void callToFriend() {
         //some code
-        System.out.println("Friend");
     }
 
     void callByNumber(int Number) {
         //some code
-        System.out.println("Phone number");
-    }
-
-    public advancedNamingConvention(int age, int phoneNumber) {
-        this.age = age;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "advancedNamingConvention{" +
-                "age=" + age +
-                ", phoneNumber=" + phoneNumber +
-                '}';
     }
 }
